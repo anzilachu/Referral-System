bcf1bc14a8c3d723cf6db39207cd7b42d5e4b427
ANZIL K <anzilmac@ANZILs-MacBook-Pro.local>
2024-04-08 00:33:42 +0530

    referrallist url

--- refapp/urls.py
+++ refapp/urls.py
@@ -4,5 +4,5 @@ from .views import UserRegistration, UserDetails,ReferralsList
 urlpatterns = [
     path('register/', UserRegistration.as_view(), name='register'),
     path('details/', UserDetails.as_view(), name='details'),
-
+    path('referrals/', ReferralsList.as_view(), name='referrals-list'),
 ]
